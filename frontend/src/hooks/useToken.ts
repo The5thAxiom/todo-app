2f446272e9509527db393a7403cea37e5cd4ab5c
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-12-06 19:11:11 +0530

    removeToken is now unsetToken

--- frontend/src/hooks/useToken.ts
+++ frontend/src/hooks/useToken.ts
@@ -4,15 +4,15 @@ import { persist } from 'zustand/middleware';
 type UseToken = {
     token: string;
     setToken: (token: string) => void;
-    removeToken: () => void;
+    unsetToken: () => void;
 };
 
 const useToken = create(
     persist<UseToken>(
         (set, get) => ({
-            token: '',
+            token: null as any,
             setToken: (token: string) => set({ token }),
-            removeToken: () => set({ token: '' })
+            unsetToken: () => set({ token: null as any })
         }),
         {
             name: 'token-storage',
